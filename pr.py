26b6b746a1ef12b61f653ab49336cb1a71c7dcd1
jaxa2289 <jaxa2289@gmail.com>
2024-12-19 02:31:17 +0500

    first commit

---
+++ pr.py
@@ -0,0 +1,7 @@
+# echo "# project_3" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/jaxa2289/project_3.git
+# git push -u origin main
